7ebfac5049c36c9942af36163a7387e2dd9ac0ea
Agris <144323153+Agris-Juskevics@users.noreply.github.com>
2024-04-11 14:50:49 +0300

    Update Webscraper.py

--- Webscraper.py
+++ Webscraper.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 import time
 
 from selenium import webdriver
@@ -9,10 +8,10 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-
+#Open microsoft by default to load page and cookie request
 stock = "MSFT"
 stockpage = f"https://finviz.com/quote.ashx?t={stock}"
-#Selenium bypasses the redirect
+#Bypasses the redirect by loading the page
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
@@ -20,21 +19,27 @@ chrome_options.add_experimental_option("prefs", {"profile.managed_default_conten
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get(stockpage)
-#Gives time for the cookie requset to appear and then bypasses it
+
+#Gives time for the cookie request to appear and then bypasses it
 time.sleep(1)
 cookies_xpath = "/html/body/div[1]/div/div/div/div[2]/div/button[2]"
 cookiebutton = driver.find_element(By.XPATH, cookies_xpath)
 cookiebutton.click()
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#Finds the stock price, absolute change and procent change
-stockprice_xpath = "/html/body/div[4]/div[3]/div[1]/div/div[1]/div[3]/div/div[2]/strong"
-stockprice = driver.find_element(By.XPATH, stockprice_xpath).text
-stockpriceabsolutechange_xpath = "/html/body/div[4]/div[3]/div[1]/div/div[1]/div[3]/div/div[2]/div/div/span[1]"
-stockpriceabsolutechange_finviz = driver.find_element(By.XPATH, stockpriceabsolutechange_xpath).text
-stockpriceabsolutechange =  f"{stockpriceabsolutechange_finviz.replace("Dollar change\n", "")}$"
-stockpriceprocentchange_xpath = "/html/body/div[4]/div[3]/div[1]/div/div[1]/div[3]/div/div[2]/div/div/span[2]"
-stockpricepprocentchange_finviz = driver.find_element(By.XPATH, stockpriceprocentchange_xpath).text
-stockpirceprocentchange = f"{stockpricepprocentchange_finviz.replace("Percentage change\n", "")}"
+
+#Finds the stock price, absolute change and procent change through xpath, because thats easier
+def update_stock_values(stonk):
+    stockpage = f"https://finviz.com/quote.ashx?t={stonk}"
+    driver.get(stockpage)
+    stockprice_xpath = "/html/body/div[4]/div[3]/div[1]/div/div[1]/div[3]/div/div[2]/strong"
+    stockprice = driver.find_element(By.XPATH, stockprice_xpath).text + "$"
+    stockpriceabsolutechange_xpath = "/html/body/div[4]/div[3]/div[1]/div/div[1]/div[3]/div/div[2]/strong"
+    stockpriceabsolutechange_finviz = driver.find_element(By.XPATH, stockpriceabsolutechange_xpath).text
+    stockpriceabsolutechange =  f"{stockpriceabsolutechange_finviz.replace("Dollar change\n", "")}$"
+    stockpriceprocentchange_xpath = "/html/body/div[4]/div[3]/div[1]/div/div[1]/div[3]/div/div[2]/div/div/span[2]"
+    stockpricepprocentchange_finviz = driver.find_element(By.XPATH, stockpriceprocentchange_xpath).text
+    stockpirceprocentchange = f"{stockpricepprocentchange_finviz.replace("Percentage change\n", "")}"
+    return stockprice,stockpriceabsolutechange, stockpirceprocentchange
 
 
-print(stockprice,stockpriceabsolutechange, stockpirceprocentchange)
+print(update_stock_values("MSFT"))
